c377a1db8f0ac467c18b61352147b59149870275
kongdefei <kdf5000@163.com>
2018-03-26 22:28:11 +0800

    login script for auth v2

--- auth.py
+++ auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding:utf-8 -*-
+# encoding=utf8 
 
 """
 目的：使用urllib2封装一个网络请求库，提供get和post方法
@@ -7,52 +7,67 @@
 import requests as req
 import hashlib
 import sys,os
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 class Net(object):
 
     @staticmethod
     def get(url,params=dict(), header=dict()):
         r = req.get(url,params=params)
+        r.encoding = "utf-8"
         return r.text
 
     @staticmethod
     def post(url, params=dict(), header=dict()):
         r = req.post(url, data=params)
+        r.encoding = "utf-8"
         return r.text
 
-def do_login():
-    pass_str = hashlib.md5("*****").hexdigest()
-    password = pass_str[8:24]
-    data = {"username":"kongdefei", "password":password,"drop":0, "type":1, "n":100}
-    headers = {"Content-Type":"application/x-www-form-urlencoded"}
-    uid = Net.post("http://159.226.39.22/cgi-bin/do_login",params=data,header=headers)
-    print uid
-    with open("uid", 'w') as fw:
-        fw.write(uid)
+def do_login(username, password):
+    #pass_str = hashlib.md5("*****").hexdigest()
+    #password = pass_str[8:24]
+    data = {"action":"login",
+            "username":username, 
+            "password":password,
+            "ac_id":1,
+            #"user_ip": "10.30.5.132",
+            #"ip": "10.30.5.132",
+            "drop":0,
+            "type": 1,
+            "n": 100,
+            "ajax": 1
+            }
 
-def do_logout():
-    if not os.path.isfile("uid"):
-        print "Please confirm that your account are on line"
-        sys.exit()
-    fp = open('uid')
-    uid = fp.readline().strip()
-    
-    if uid == "":
-        print "Please confirm that your account are on line"
-        sys.exit()
-
-    data = {"uid":uid}
     headers = {"Content-Type":"application/x-www-form-urlencoded"}
-    msg = Net.post("http://159.226.39.22/cgi-bin/do_logout",params=data, header=headers)
+    res = Net.post("https://gw.ict.ac.cn/srun_portal_pc.php?ac_id=1&",params=data,header=headers)
+    print res
+
+def do_logout(username, password):
+    data = {
+            "action":"logout",
+            "username":username,
+            "password":password,
+            "drop":0,
+            "type":1,
+            "n":1,
+            "ajax":1,
+    }
+
+    headers = {"Content-Type":"application/x-www-form-urlencoded"}
+    msg = Net.post("https://gw.ict.ac.cn/srun_portal_pc.php?ac_id=1&",params=data, header=headers)
     print msg
 
 if __name__ == "__main__":
-    if len(sys.argv) <= 1:
-        print "Usage: python auth.py [login|logout]"
+    if len(sys.argv) < 4:
+        print "Usage: python auth.py [login|logout] username password"
         sys.exit()
     cmd = sys.argv[1]
+    username = sys.argv[2]
+    password = sys.argv[3]
     if cmd == 'login':
         # print "Login..."
-        do_login()
+        do_login(username,password)
     else:
         # print "Logout..."
-        do_logout()
+        do_logout(username, password)
--- mornitor_net.sh
+++ mornitor_net.sh
@@ -2,7 +2,7 @@
 
 ping -c 3 -t 5 www.baidu.com > /dev/null
 if [ $? != 0 ];then
-   ret=$(/usr/bin/env python /home/pi/Documents/Coding/ICTAuth/auth.py login)
+   ret=$(/usr/bin/env python /home/pi/Documents/Coding/ICTAuth/auth.py login kongdefei devin123)
    echo "[`date`]: Reconnect to ICT, uid: $ret"
 else
    echo "[`date`]: Connected to the internet"
